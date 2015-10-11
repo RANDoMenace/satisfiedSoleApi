4c899cd8d6afb2c295a55e66249abca26c782c84
Taylor Harwood <htaylor127@gmail.com>
2015-10-11 14:51:56 -0700

    ignore config file

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  'port': process.env.PORT || 3000,
-  'databaseURI': process.env.MONGOLAB_URI || 'mongodb://localhost/satisfiedSole_api',
-  'ebayAPIKey' :
-  'secret': 'satisfyMySole'
-};
